46afb14cf58c48a0b04c34f5163fbf9908aabd4c
John Gietzen <otac0n@gietzen.us>
2009-11-20 23:11:13 +0000

    git-svn-id: http://otac0n.com:3000/projects/SharpCash/trunk@915 14c0b8bc-c9cf-8f41-ba33-e04da095c157

---
+++ SharpCashDebug/pmt.py
@@ -0,0 +1,32 @@
+def pmt(i, l, p, m, n):
+    return p * ( i / (1 - (1 + i) ** -l))
+
+def ipmt(i, num, l, p, a, b):
+    m = pmt(i, l, p, a, b)
+    n = 1
+    while True:
+        h = p * i
+        if (n == num):
+            return h
+        c = m - h
+        p = p - c
+        if (p < 0):
+            return 0
+        n = n + 1
+
+def ppmt(i, num, l, p, a, b):
+    m = pmt(i, l, p, a, b)
+    n = 1
+    while True:
+        h = p * i
+        c = m - h
+        if (n == num):
+            return c
+        p = p - c
+        if (p < 0):
+            return 0
+        n = n + 1
+
+print pmt(0.05 / 12, 360, 82478, 0, 0)
+print ipmt(0.05 / 12, 600, 360, 82478, 0, 0)
+print ppmt(0.05 / 12, 600, 360, 82478, 0, 0)
