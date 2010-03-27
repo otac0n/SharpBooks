e21dcaea88dddb8fe8c7e8058820654d6321e662
otac0n <otac0n@gietzen.us>
2010-03-27 17:43:21 +0000

    git-svn-id: http://otac0n.com:3000/projects/SharpBooks/trunk@1263 14c0b8bc-c9cf-8f41-ba33-e04da095c157

---
+++ YahooPriceSource/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("YahooPriceSource")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("YahooPriceSource")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d8f1273a-7146-49d2-a093-12d95a9d2261")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ YahooPriceSource/YahooPriceSource.cs
@@ -0,0 +1,12 @@
+﻿namespace YahooPriceSource
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class YahooPriceSource
+    {
+        private const string UrlFormat = "http://finance.yahoo.com/d/quotes.csv?f=sd1l1&s={0}";
+    }
+}
