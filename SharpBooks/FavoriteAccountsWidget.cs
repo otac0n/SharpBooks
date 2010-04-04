cbe27eaab96a2cf2e9ad0fb28314cd881ffa664a
otac0n <otac0n@gietzen.us>
2010-04-04 19:34:30 +0000

    git-svn-id: http://otac0n.com:3000/projects/SharpBooks/trunk@1330 14c0b8bc-c9cf-8f41-ba33-e04da095c157

--- SharpBooks/FavoriteAccountsWidget.cs
+++ SharpBooks/FavoriteAccountsWidget.cs
@@ -8,11 +8,11 @@
 namespace SharpBooks
 {
     using System;
+    using System.Collections.Generic;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Input;
     using SharpBooks.Plugins;
-using System.Collections.Generic;
 
     internal class FavoriteAccountsWidget : IWidget
     {
