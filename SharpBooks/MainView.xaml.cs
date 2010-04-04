bcb99c2af2ac23e46edc91c0a6fedc355eb4ee81
otac0n <otac0n@gietzen.us>
2010-04-04 02:20:37 +0000

    git-svn-id: http://otac0n.com:3000/projects/SharpBooks/trunk@1326 14c0b8bc-c9cf-8f41-ba33-e04da095c157

--- SharpBooks/MainView.xaml.cs
+++ SharpBooks/MainView.xaml.cs
@@ -24,11 +24,14 @@
             InitializeComponent();
 
             var factory = new FavoriteAccountsWidgetFactory();
-            for (var i = 0; i < 100; i++)
+            for (var i = 0; i < 3; i++)
             {
                 var widget = factory.CreateInstance(null, null);
                 var expander = new Expander
                 {
+                    IsExpanded = true,
+                    BorderBrush = new SolidColorBrush(Color.FromRgb(0, 0, 0)),
+                    Padding = new Thickness(2.0d),
                     Margin = new Thickness(5.0d),
                     Header = factory.Name,
                     Content = widget.Create(null, null)
