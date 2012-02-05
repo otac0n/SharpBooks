5543f713096498cc700334d6cf681e9dbaae2a56
John Gietzen <otac0n@gietzen.us>
2012-02-05 12:05:41 -0600

    Added tests for `Contains` and implemented `Contains` for ranges containing ranges.

--- SharpBooks.SharedLibrary/Integration/RangeExtensions.cs
+++ SharpBooks.SharedLibrary/Integration/RangeExtensions.cs
@@ -35,5 +35,58 @@ namespace SharpBooks.Integration
 
             return true;
         }
+
+        public static bool Contains<T>(this IRange<T> range, IRange<T> other, bool startInclusive = true, bool endInclusive = false) where T : IComparable<T>
+        {
+            if (range.Start.CompareTo(range.End) > 0 ||
+                other.Start.CompareTo(other.End) > 0)
+            {
+                return false;
+            }
+
+            var startToStart = range.Start.CompareTo(other.Start);
+            var endToEnd = range.End.CompareTo(other.End);
+
+            if (startToStart > 0 || // The other range start before this range.
+                endToEnd < 0)       // The other range ends after this range.
+            {
+                return false;
+            }
+
+            var startToEnd = range.Start.CompareTo(other.End);
+            var endToStart = range.End.CompareTo(other.Start);
+
+            if (startToStart < 0)
+            {
+                // The other range starts after this one does.
+
+                if (endToStart == 0 && !endInclusive)
+                {
+                    // The other range ends when this one ends, but the end is not inclusive.
+                    return false;
+                }
+            }
+
+            if (endToEnd > 0)
+            {
+                // The other range ends before this one does.
+
+                if (startToEnd == 0 && !startInclusive)
+                {
+                    // The other range starts when this one ends, but the start is not inclusive.
+                    return false;
+                }
+            }
+
+            if (startToEnd == 0 &&
+                !startInclusive &&
+                !endInclusive)
+            {
+                // Both ranges include exactly one point, but the point is not inclusive on either end.
+                return false;
+            }
+
+            return true;
+        }
     }
 }
