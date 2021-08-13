67e6b56ad8d43b232cf564ba611386a91b6402bd
John Gietzen <john@gietzen.us>
2021-08-12 23:34:36 -0700

    Stub out GNUCash integration.

---
+++ GnuCashIntegration/PluginEunmerator.cs
@@ -0,0 +1,22 @@
+// Copyright © John Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
+
+namespace GnuCashIntegration
+{
+    using System.Collections.Generic;
+    using SharpBooks.Plugins;
+
+    /// <summary>
+    /// Enumerates all of the plugins for the assembly.
+    /// </summary>
+    public class PluginEunmerator : IPluginEnumerator
+    {
+        /// <summary>
+        /// Enumerates all of the plugins for the assembly.
+        /// </summary>
+        /// <returns>An enumerable list of factories that can create plugins.</returns>
+        public IEnumerable<IPluginFactory> EnumerateFactories()
+        {
+            yield return new PluginFactory<GnuCashDataSource>("GNUCash Data Source");
+        }
+    }
+}
